38f10a664611a0865a4f37fd04302e818772d5fc
noah bean <noahbean3396@gmail.com>
2026-01-05 14:59:51 -0800

    [any message]

--- src/main.c
+++ src/main.c
@@ -77,13 +77,13 @@ int main() {
 		char *response = "HTTP/1.1 200 OK\r\n\r\n";
 		send(client_fd, response, strlen(response), 0);
 	}
-	else if (strncmp(http_path, "/echo", 6) == 0)
+	else if (strncmp(http_path, "/echo/", 6) == 0)
 	{
 		char *content = http_path + 6;
 		size_t content_len = strlen(content);
 		char response[1024];
 		snprintf(response, sizeof(response), "HTTP/1.1 200 OK\r\n" "Content-Type: text/plain\r\n"
-		"Content-Length: %zu\r\n" "\r\n" "%s", content_length, content);
+		"Content-Length: %zu\r\n" "\r\n" "%s", content_len, content);
 		send(client_fd, response, strlen(response), 0);
 
 	}
