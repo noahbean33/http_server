c40019bee3b7d2b8c582b502bd8c57f0f09c999b
noah bean <noahbean3396@gmail.com>
2026-01-05 13:24:45 -0800

    [any message]

--- src/main.c
+++ src/main.c
@@ -53,9 +53,16 @@ int main() {
 	printf("Waiting for a client to connect...\n");
 	client_addr_len = sizeof(client_addr);
 	
-	accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+	int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+	if (client_fd == -1) {
+		printf("Accept failed: %s\n", strerror(errno));
+		close(server_fd);
+		return 1;
+	}
 	printf("Client connected\n");
 	
+	char *res = "HTTP/1.1 200 OK\r\n\r\n";
+	send(server_fd, res, strlen(res), 0);
 	close(server_fd);
 
 	return 0;
