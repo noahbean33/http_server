20b2129177f042e49c73dcb61dd1e4a1bf362b75
noah bean <noahbean3396@gmail.com>
2026-01-06 17:00:44 -0800

    [any message]

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <pthread.h>
 
 char * extract_header_value(char *headers, const char *header_name) 
 {
@@ -31,6 +32,72 @@ char * extract_header_value(char *headers, const char *header_name)
 	return NULL;
 }
 
+void *handle_client(void *arg) {
+	int client_fd = *(int *)arg;
+	free(arg);
+	
+	char request[1024];
+	ssize_t bytes_read = recv(client_fd, request, sizeof(request), 0);
+	
+	if (bytes_read <= 0) {
+		close(client_fd);
+		return NULL;
+	}
+	
+	char *http_method = NULL;
+	char *http_path = NULL;
+	char *saveptr = NULL;
+	char *line_saveptr = NULL;
+	
+	http_method = strtok_r(request, "\r\n", &saveptr);
+	http_path = strtok_r(http_method, " ", &line_saveptr);
+	http_path = strtok_r(NULL, " ", &line_saveptr);
+	
+	if (strcmp(http_path, "/") == 0)
+	{
+		char *response = "HTTP/1.1 200 OK\r\n\r\n";
+		send(client_fd, response, strlen(response), 0);
+	}
+	else if (strncmp(http_path, "/echo/", 6) == 0)
+	{
+		char *content = http_path + 6;
+		size_t content_len = strlen(content);
+		char response[1024];
+		snprintf(response, sizeof(response), "HTTP/1.1 200 OK\r\n" "Content-Type: text/plain\r\n"
+		"Content-Length: %zu\r\n" "\r\n" "%s", content_len, content);
+		send(client_fd, response, strlen(response), 0);
+	}
+	else if (strcmp(http_path, "/user-agent") == 0)
+	{
+		char *user_agent = extract_header_value(saveptr, "User-Agent");
+		
+		if (user_agent) {
+			size_t content_len = strlen(user_agent);
+			char response[1024];
+			snprintf(response, sizeof(response), 
+				"HTTP/1.1 200 OK\r\n"
+				"Content-Type: text/plain\r\n"
+				"Content-Length: %zu\r\n"
+				"\r\n"
+				"%s", 
+				content_len, user_agent);
+			send(client_fd, response, strlen(response), 0);
+			free(user_agent);
+		} else {
+			char *response = "HTTP/1.1 400 Bad Request\r\n\r\n";
+			send(client_fd, response, strlen(response), 0);
+		}
+	}
+	else
+	{
+		char *response = "HTTP/1.1 404 Not Found\r\n\r\n";
+		send(client_fd, response, strlen(response), 0);
+	}
+	
+	close(client_fd);
+	return NULL;
+}
+
 int main() {
 	// Disable output buffering
 	setbuf(stdout, NULL);
@@ -39,10 +106,7 @@ int main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	printf("Logs from your program will appear here!\n");
 
-	// TODO: Uncomment the code below to pass the first stage
-	
-	int server_fd, client_addr_len;
-	struct sockaddr_in client_addr;
+	int server_fd;
 	
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd == -1) {
@@ -74,73 +138,36 @@ int main() {
 		return 1;
 	}
 	
-	printf("Waiting for a client to connect...\n");
-	client_addr_len = sizeof(client_addr);
+	printf("Waiting for clients to connect...\n");
 	
-	int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-	if (client_fd == -1) {
-		printf("Accept failed: %s\n", strerror(errno));
-		close(server_fd);
-		return 1;
-	}
-	printf("Client connected\n");
-	
-	char request[1024];
-	ssize_t bytes_read = recv(client_fd, request, sizeof(request), 0);
-
-	char *http_method = NULL;
-	char * http_path = NULL;
-	char *saveptr = NULL;
-	char *line_saveptr = NULL;
-
-	http_method = strtok_r(request, "\r\n", &saveptr);
-	http_path = strtok_r(http_method, " ", &line_saveptr);
-	http_path = strtok_r(NULL, " ", &line_saveptr);
-
-	if (strcmp(http_path, "/") == 0)
-	{
-		char *response = "HTTP/1.1 200 OK\r\n\r\n";
-		send(client_fd, response, strlen(response), 0);
-	}
-	else if (strncmp(http_path, "/echo/", 6) == 0)
-	{
-		char *content = http_path + 6;
-		size_t content_len = strlen(content);
-		char response[1024];
-		snprintf(response, sizeof(response), "HTTP/1.1 200 OK\r\n" "Content-Type: text/plain\r\n"
-		"Content-Length: %zu\r\n" "\r\n" "%s", content_len, content);
-		send(client_fd, response, strlen(response), 0);
-
-	}
-	else if (strcmp(http_path, "/user-agent") == 0)
-	{
-		// Extract User-Agent header value from the remaining request
-		char *user_agent = extract_header_value(saveptr, "User-Agent");
+	// Infinite loop to handle multiple concurrent connections
+	while (1) {
+		struct sockaddr_in client_addr;
+		socklen_t client_addr_len = sizeof(client_addr);
 		
-		if (user_agent) {
-			size_t content_len = strlen(user_agent);
-			char response[1024];
-			snprintf(response, sizeof(response), 
-				"HTTP/1.1 200 OK\r\n"
-				"Content-Type: text/plain\r\n"
-				"Content-Length: %zu\r\n"
-				"\r\n"
-				"%s", 
-				content_len, user_agent);
-			send(client_fd, response, strlen(response), 0);
-			free(user_agent);
-		} else {
-			// No User-Agent header found
-			char *response = "HTTP/1.1 400 Bad Request\r\n\r\n";
-			send(client_fd, response, strlen(response), 0);
+		int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+		if (client_fd == -1) {
+			printf("Accept failed: %s\n", strerror(errno));
+			continue;
 		}
+		
+		printf("Client connected\n");
+		
+		// Create a new thread for this client
+		pthread_t thread_id;
+		int *pclient = malloc(sizeof(int));
+		*pclient = client_fd;
+		
+		if (pthread_create(&thread_id, NULL, handle_client, pclient) != 0) {
+			printf("Failed to create thread\n");
+			close(client_fd);
+			free(pclient);
+			continue;
+		}
+		
+		pthread_detach(thread_id);
 	}
-	else
-	{
-		char *response = "HTTP/1.1 404 Not Found\r\n\r\n";
-		send(client_fd, response, strlen(response), 0);
-	}
-	close(client_fd);
-
+	
+	close(server_fd);
 	return 0;
 }
